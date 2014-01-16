298393adf869717e17e9a77fae2f3bbc92d6866b
Ray Mitchell <raymonxo@gmail.com>
2014-01-15 16:55:46 -0800

    Refactored problem 1.

--- project_euler/problem1/problem1.rb
+++ project_euler/problem1/problem1.rb
@@ -1,2 +1,8 @@
 # Project Euler, Problem #1 (http://projecteuler.net/problem=1)
-puts (1..999).select { |a| a % 3 == 0 || a % 5 == 0 }.reduce :+
+class Integer < Numeric
+  def has_factor?(divisor)
+    self % divisor == 0
+  end
+end
+
+puts (1..999).select { |a| a.has_factor?(3) || a.has_factor?(5) }.reduce :+
